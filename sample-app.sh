79e872191e24b055e9e527ccea3ff2e903b585fc
Julián Reyes Reveco <julianreyes28092001@gmail.com>
2026-08-07 23:45:35 -0400

    Docker y CI/CD

--- sample-app.sh
+++ sample-app.sh
@@ -1,31 +1,22 @@
 #!/bin/bash
 
-rm -rf tempdir
-mkdir -p tempdir/templates
-mkdir -p tempdir/static
+mkdir tempdir
+mkdir tempdir/templates
+mkdir tempdir/static
 
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
 
-echo "FROM python:3.10" > tempdir/Dockerfile
-echo "RUN pip install --progress-bar off flask" >> tempdir/Dockerfile
-echo "COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+echo "FROM python" >> tempdir/Dockerfile
+echo "RUN pip install flask" >> tempdir/Dockerfile
+echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
+echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
 echo "EXPOSE 8080" >> tempdir/Dockerfile
-echo 'CMD ["python", "/home/myapp/sample_app.py"]' >> tempdir/Dockerfile
+echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
 
 cd tempdir
-
-echo "========== DOCKERFILE =========="
-cat Dockerfile
-echo "================================"
-
-docker build --no-cache -t sampleapp .
-
-docker rm -f samplerunning 2>/dev/null || true
-
-docker run -t -d -p 8888:8080 --name samplerunning sampleapp python /home/myapp/sample_app.py
-
+docker build -t sampleapp .
+docker run -t -d -p 8888:8080 --name samplerunning sampleapp
 docker ps -a
